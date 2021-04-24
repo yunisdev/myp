9b89e942f4528ad5f20f65f0b5e805d36c00d742
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-04-24 18:00:35 +0400

    updated flaskmng template

--- python_script_manager/templates/flaskmng.py
+++ python_script_manager/templates/flaskmng.py
@@ -10,12 +10,16 @@ flaskmng = {
                 "description":"Removing app from project."
             },
             "mkmig":{
-                "command":"flask db commit {args}",
+                "command":"flask db migrate {args}",
                 "description":"Make migrations for project."
             },
             "mig":{
                 "command":"flask db upgrade",
                 "description":"Upgrade db for project."
+            },
+            "dbinit":{
+                "command":"flask db init",
+                "description":"Initialize db for project."
             },
             "start":{
                 "command":"flask run",
