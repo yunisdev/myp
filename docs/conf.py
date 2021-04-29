5a2a36888b2dcd15402624f872b4ac1a7dc29935
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-04-29 18:19:16 +0400

    new docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '1.3.1'
 # ones.
 extensions = [
     'sphinx_click',
-    "sphinx_rtd_theme",
+    'insegel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,13 +49,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'insegel'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_theme_options={
-    "nosidebar":True
-}
