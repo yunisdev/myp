a2e5a1f24c292f2e8a625737020bf35465f33240
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-04-29 17:37:49 +0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ release = '1.3.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_click'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
