53c0259f471950e4948fba0b783e7486bf444f96
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-05-02 19:12:09 +0400

    favicon

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_favicon = 'myp_icon.png'
 html_logo = "myp_logo.svg"
 
 html_theme_options={
